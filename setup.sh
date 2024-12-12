5b42d01bb8b1a133322ded7925b7ed6e0256e0d7
Prashant Abhishek <prashant.abhishek7g@gmail.com>
2024-12-12 13:14:05 +0530

    downgrade chroma

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Install latest SQLite from source
 wget https://www.sqlite.org/2024/sqlite-autoconf-3420000.tar.gz
 tar -xvf sqlite-autoconf-3420000.tar.gz
 cd sqlite-autoconf-3420000
