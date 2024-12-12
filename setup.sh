816c32ee60a418a4aa269a080d07197f7362e546
Prashant Abhishek <prashant.abhishek7g@gmail.com>
2024-12-12 13:08:35 +0530

    add sqlite

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget https://www.sqlite.org/2024/sqlite-autoconf-3420000.tar.gz
+tar -xvf sqlite-autoconf-3420000.tar.gz
+cd sqlite-autoconf-3420000
+./configure --prefix=/usr
+make
+make install
