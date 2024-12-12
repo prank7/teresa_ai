f63cd3adfdd3fe7f4a9d4608c21111077c93089d
Prashant Abhishek <prashant.abhishek7g@gmail.com>
2024-12-12 13:36:32 +0530

    add new package

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# Download and install SQLite 3.42.0 (adjust if newer versions exist)
-wget https://www.sqlite.org/2024/sqlite-autoconf-3420000.tar.gz
+
+# Download SQLite source using curl
+curl -O https://www.sqlite.org/2024/sqlite-autoconf-3420000.tar.gz
+
+# Extract and build SQLite
 tar -xvf sqlite-autoconf-3420000.tar.gz
 cd sqlite-autoconf-3420000
 ./configure --prefix=/usr
